6065dfc603d0f82cdbd94178641dc3485ded7753
yacinebenkaidali <fy_benkaidali@esi.dz>
2019-04-15 11:52:53 +0100

    Added phone call stil the images display

--- app/src/main/java/com/esi/projetmobile/Adapter/RealEstateAdapter.kt
+++ app/src/main/java/com/esi/projetmobile/Adapter/RealEstateAdapter.kt
@@ -1,15 +1,20 @@
 package com.esi.projetmobile.Adapter
 
+import android.Manifest
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.esi.projetmobile.Adapter.RealEstateAdapter.ViewHolder
 import com.esi.projetmobile.Model.RealEstate
@@ -44,12 +49,7 @@ class RealEstateAdapter(realEstateList: MutableList<RealEstate>, context: Contex
             Navigation.findNavController(it).navigate(R.id.action_ads_to_DetailFragment, args)
         }
         p0.itemCard.setOnClickListener {
-            val gmmIntentUri = Uri.parse(realEstate.coordinates)
-            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
-            mapIntent.setPackage("com.google.android.apps.maps")
-            if (mapIntent.resolveActivity(context.packageManager) != null) {
-                context.startActivity(mapIntent)
-            }
+            displayDialog(realEstate)
         }
 
     }
@@ -109,5 +109,40 @@ class RealEstateAdapter(realEstateList: MutableList<RealEstate>, context: Contex
             (real1.squareFootage - real2.squareFootage).toInt()
 
         })
+    }
+
+    private fun displayDialog(realEstate: RealEstate) {
+        val dialogBuilder = AlertDialog.Builder(context)
+        dialogBuilder.setTitle("Choose your action ?")
+        dialogBuilder.setMessage("Which action u want to go to ?!")
+            .setCancelable(false)
+            .setPositiveButton("Google Maps") { dialog, id ->
+                val gmmIntentUri = Uri.parse(realEstate.coordinates)
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                if (mapIntent.resolveActivity(context.packageManager) != null) {
+                    context.startActivity(mapIntent)
+                }
+            }
+            .setNegativeButton("Call his/her Phone") { dialog, id ->
+                val callIntent = Intent(Intent.ACTION_CALL)
+                callIntent.data = Uri.parse("tel:" + 8802177690)//Owner's phone number
+                if (ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.CALL_PHONE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+//                    ActivityCompat.requestPermissions(context as Activity
+//                        ,  arrayOf(Manifest.permission.CALL_PHONE),);
+                    // TODO check this one later
+                    Toast.makeText(context, "grant me permissions", Toast.LENGTH_LONG).show()
+                } else {
+                    context.startActivity(callIntent)
+                }
+
+            }
+        val alert = dialogBuilder.create()
+        alert.setTitle("AlertDialogExample")
+        alert.show()
     }
 }
